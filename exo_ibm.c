a9716036d005dadd25ca98c06832329e30c3b288
Adrian Bach <adrian.bach@stir.ac.uk>
2019-03-06 18:50:53 +0100

    first draft update_death

--- exo_ibm.c
+++ exo_ibm.c
@@ -15,11 +15,79 @@ int popInit = 10;
 // // birth probability
 // float pB = 0.1;
 
-// // death probability 
-// float pD = 0.1;
+// death probability 
+float pD = 0.1;
 
 // // maximal moving distance (could be a proportion of world's size)
 // int maxDist = 2;
+
+// death function
+// takes pop_table in and updates it
+void update_death(int **tab_in)
+{
+  // count of the deaths
+  int deaths = 0;
+
+  // while/for loop on individuals in pop_table
+  for (int row = 0; row < pop_size; ++row)
+  {
+    // test for death
+    // generate a random number between 0 and 1
+    float r = rand()/(RAND_MAX);
+
+    // test
+    if (r < pD)
+    {
+      *(*(tab_in + row) + 2) = 0;
+      deaths += 1;
+    }
+
+    // update pop_size
+    pop_alive = pop_size - deaths; 
+
+    // create a new table with the right size
+    // pointer to pointers
+    int **tab_out;
+  
+    // allocate memory
+    tab_out = malloc(pop_alive * sizeof(int *));
+    for(int row = 0; row < pop_alive; ++row)
+    {
+      *(tab_out + row) = malloc(col_number * sizeof(int));
+    }
+
+    // track the living and copy their values to the new table
+    for (int row = 0; row < popInit; ++row)
+    {   
+      // browse the livings and store them in the new table
+      if (*(*(tab_in + row) + 2 == 1)
+      {
+        for (int col = 0; col < col_number; ++col)
+        {
+          *(*(tab_out + row) + col = *(*(tab_in + row) + col;
+        }
+      }
+    }
+
+    // free the old table
+    for(int row = 0; row < pop_size; ++row)
+    {
+      free(*(tab_in + row));
+    }
+    free(tab_in);
+
+    // rename the new table correctly ?
+
+    //return(0); 
+  }
+}
+
+// birth function for the survivors
+// takes pop_table in and updates it
+void update_birth(int **tab_in)
+{
+  /* code */  
+}
 
 int main(int argc, char const *argv[])
 {
@@ -106,95 +174,11 @@ int main(int argc, char const *argv[])
   // initiate a variable for the size of pop_table
   int pop_size = popInit;
 
-  // // death function
-  // // takes pop_table in and returns a new version of it
-  /* type of return */ update_birth (/* pop_table */)
+  // for/while loop on time steps
+  for (int t = 0; t < maxTs; ++t)
   {
-    // // count of the deaths
-    // int deaths = 0;
-
-    // while/for loop on individuals in pop_table
-    for (int row = 0; row < pop_size; ++row)
-    {
-      // test for death
-      // generate a random number between 0 and 1
-      float test = rand()/(RAND_MAX+1.0);
-
-      // test
-      if (test < pB)
-      {
-        *(*(pop_table + row) + 2) = 0;
-        deaths += 1;
-      }
-    }
-
-    // update pop_size
-    pop_size -= deaths; 
-
-    // create a new table with the right size
-    // pointer to pointers
-    int **temp_table;
- 
-    // as many column as variables 
-    int temp_col_nb = 3;
-  
-    // allocate memory
-    temp_table = malloc(pop_size * sizeof(int *));
-    for(int row = 0; row < pop_size; ++row)
-    {
-      *(temp_table + row) = malloc(temp_col_nb * sizeof(int));
-    }
-
-    for (int row = 0; row < popInit; ++row)
-    {   
-      // random values for the first two columns
-      // generate 2 random numbers between 0 and S included
-      int r1 = rand() % (S+1);
-      int r2 = rand() % (S+1);
-
-      // assign it
-      *(*(pop_table + row) + 0) = r1;
-      *(*(pop_table + row) + 1) = r2;
-
-      // every ind is alive at first
-      *(*(pop_table + row) + 2) = 1;
-    }
-
-    // track the living and copy their values to the new table
-
-    // free the old table
-
-    // rename the old table correctly
-
-    return(/* pop_table */); 
+    /* update successively pop_table with the functions */
   }
-
-  // // birth function for the survivors
-  // // takes pop_table in and returns a new version of it
-  // /* type of return */ update_birth (/* arguments ? */)
-  // {
-  //   // while/for loop on individuals in pop_table
-  //   // how many rows?
-
-
-  //   // test for birth
-  //   // generate a random number between 0 and 1
-  //   float test = rand()/(RAND_MAX+1.0);
-
-  //   // test
-  //   if (test > pB)
-  //   {
-  //     return(** pop_);
-  //   }
-
-  //   // allocate memory for a new line at the end of the table
-
-  //   // assign same position and a 1 to the DoA column
-
-  //   // update pop_table
-
-  //   return(pop_table); 
-  // }
 
   // free memory in the end
   for(int row = 0; row < popInit; ++row){
@@ -204,7 +188,3 @@ int main(int argc, char const *argv[])
 
 	return 0;
 }
-
-// // generate a random number between 0 and 1
-// int r = rand();
-// float test = r/(RAND_MAX+1.0);
